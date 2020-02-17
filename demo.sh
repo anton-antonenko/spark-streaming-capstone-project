a289051715e13dcbeb9fbd846d040b3dad4ed365
anton.antonenko <anton_antonenko@homedepot.com>
2020-02-17 10:22:27 +0200

    a bit more instructions

--- demo.sh
+++ demo.sh
@@ -11,17 +11,17 @@ docker ps
 docker exec -it <containerId> bash
   # go to dir with conenctor configs
   cd /files/kafka_connect
-  # create the topic we're gonna write to with 3 partitions
-  kafka-topics --create --topic click-stream --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
+  # create a topic which we're gonna write to with 8 partitions (or if you computer has a different number of cores - use this number)
+  kafka-topics --create --topic click-stream --partitions 8 --replication-factor 1 --zookeeper 127.0.0.1:2181
   # create a connector
   curl -X POST -H "Content-Type: application/json" --data @file_connector_conf.json http://localhost:8083/connectors
   # shut down the terminal
-  # open in browser http://localhost:3030/kafka-connect-ui/#/cluster/fast-data-dev and make sure the connector has been created
+  # open in a browser http://localhost:3030/kafka-connect-ui/#/cluster/fast-data-dev and make sure the connector has been created
 
-# start data generator
+# start the data generator
 # it's supposed that it's already built. if not, go to the `./data-generator` and run `sbt assembly`
 java -jar ./data-generator/target/data-generator-1.0-SNAPSHOT.jar --usersNum=100 --eventsPerSecond=3000
 
-# start Spark job
+# start the Spark job
 # the same - if it's not built yet, go to the `./spark-job` and run `sbt assembly`
 java -jar ./spark-job/target/spark-job-1.0-SNAPSHOT.jar
