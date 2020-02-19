59eb77d496a7ef34b4342028840ef31839a9aa5c
anton.antonenko <anton_antonenko@homedepot.com>
2020-02-19 18:57:03 +0200

    add +x to executables; rename assembly.sbt -> plugins.sbt

--- spark-job/src/main/scala/com/gridu/aantonenko/streaming/StreamingJob.scala
+++ spark-job/src/main/scala/com/gridu/aantonenko/streaming/StreamingJob.scala
@@ -3,11 +3,11 @@ package com.gridu.aantonenko.streaming
 import java.sql.Timestamp
 
 import com.datastax.driver.core.utils.UUIDs
-import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
+import org.apache.spark.sql.{ Dataset, Row, SaveMode, SparkSession }
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.OutputMode
-import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
-import org.slf4j.{Logger, LoggerFactory}
+import org.apache.spark.sql.types.{ LongType, StringType, StructField, StructType }
+import org.slf4j.{ Logger, LoggerFactory }
 import scopt.OptionParser
 
 object StreamingJob {
